443d26d34488ad995818ce99bbbc86a74d0fbc78
berlot83 <berlot83@yahoo.com.ar>
2018-02-13 16:58:16 -0300

    Estilos en login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { TabsPage } from '../tabs/tabs';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {TabsPage} from '../tabs/tabs';
+import {AlertController} from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -10,17 +11,64 @@ import { TabsPage } from '../tabs/tabs';
 export class LoginPage {
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  usuario: string;
+  password: string;
+  constructor(private alertCtrl: AlertController, public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
 
-  login(){
-      if(this.usuario == this.password){
-          this.navCtrl.push(TabsPage);
-      }
+  login() {
+ 
+    if (this.usuario == this.password) {
+      this.navCtrl.push(TabsPage);
+    }
   }
+
+  /*
+   * AAB 13/03/2018
+   * Function js
+   * on login.html
+   * forgot password link
+   */
   
+  forgotPass() {
+    let alert = this.alertCtrl.create({
+      title: 'Recuperar Password',
+      inputs: [
+        {
+          name: 'Email',
+          placeholder: 'Email',
+          type: 'email'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Login',
+          handler: data => {
+            if (User.isValid(data.email)) {
+              // send recovery email to
+            } else {
+                alert= this.alertCtrl.create({
+                title: 'Error',
+                subTitle: 'El email no pertenece a un usuario activo'
+                });
+              return false;
+            }
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
 }
